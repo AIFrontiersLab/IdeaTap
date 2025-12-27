08f15f49347ada0a604c2332748ce9029116ed84
Aswini <aswini.atbd@gmail.com>
2025-12-27 13:23:00 -0800

    Initial Commit

---
+++ IdeaTapTests/IdeaTapTests.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaTapTests.swift
+//  IdeaTapTests
+//
+//  Created by Aswini on 12/27/25.
+//
+
+import Testing
+@testable import IdeaTap
+
+struct IdeaTapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
